04d8624bff3aac8738387da73a678a202cb6e802
Amrutha Yarlagadda <amrutha1827@gmail.com>
2023-09-29 20:52:02 -0400

    Added personal-budget

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className='header-budget'>
+            <h1  className='app-name'>Personal Budget Application</h1>
+            <h2 className='app-description'>Spend, save, and give toward what's important in life by best managing your budget</h2>
+         </div>
+  );
+}
+
+export default Header;
---
+++ src/Menu/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  Link
+} from "react-router-dom";
+
+function Menu() {
+  return (
+    <nav 
+       role = "navigation"
+       aria-label="Main menu"
+       itemScope
+      itemType=''
+    >
+    <ul>
+       <li><Link to="/">Home</Link></li>
+       <li><Link to="/about">About</Link></li>
+       <li><Link to="/login">Login</Link></li>
+    </ul>
+  </nav>
+  );
+}
+
+export default Menu;
